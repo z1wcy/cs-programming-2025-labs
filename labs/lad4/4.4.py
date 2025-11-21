01e391bd19488a78be3be316d29f9fa6e78b6751
z1wcy <kirijcukm@gmail.com>
2025-11-21 12:49:45 +1000

    ffff

--- labs/lad4/4.4.py
+++ labs/lad4/4.4.py
@@ -1,3 +1,5 @@
 num=input("Введите число: ")
-if int(num[-1]) %2==0 and sum(map(int,num))%3==0: print("число делится на 6")
-else: print("Число не делится на 6")
+if int(num[-1]) %2==0 and sum(map(int,num))%3==0: 
+    print("число делится на 6")
+else: 
+    print("Число не делится на 6")
--- labs/lad4/4.5.py
+++ labs/lad4/4.5.py
@@ -1,9 +1,15 @@
 pas= input("введите пароль: ")
 def chek(password):
-    if len(password)<8: return print("пароль должен содержать не менее 8 символов")
-    if password.lower()== password: return print("пароль должен содержать хотябы 1 заглавный символ")
-    if password.upper()== password: return print("пароль должен содержать хотябы 1 символ нижнего регистра")
-    if not any(x.isdigit() for x in password): return print("пароль должен содержать хотябы 1 цифру")
-    if not any(x.isalnum() for x in password): return print("пароль должен содержать хотябы 1 специальный символ")
-    else: return print("пароль установлен")
+    if len(password)<8: 
+        return print("пароль должен содержать не менее 8 символов")
+    if password.lower()== password: 
+        return print("пароль должен содержать хотябы 1 заглавный символ")
+    if password.upper()== password: 
+        return print("пароль должен содержать хотябы 1 символ нижнего регистра")
+    if not any(x.isdigit() for x in password): 
+        return print("пароль должен содержать хотябы 1 цифру")
+    if not any(x.isalnum() for x in password): 
+        return print("пароль должен содержать хотябы 1 специальный символ")
+    else:  
+        print("пароль установлен")
 print(chek(pas))
