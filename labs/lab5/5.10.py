abe0e7c4c54d6a414b75c547bd74888c39ffffae
z1wcy <kirijcukm@gmail.com>
2025-11-28 10:30:58 +1000

    ss

--- labs/lab5/5.10.py
+++ labs/lab5/5.10.py
@@ -1,13 +1,13 @@
-x = [("Анна", [5, 4, 5]), ("Иван", [3, 4, 4]), ("Мария", [5, 5, 5])]
-f = {}
+students = [("Анна", [5, 4, 5]), ("Иван", [3, 4, 4]), ("Мария", [5, 5, 5])]
 
-for i in x:
-   q,w = i
-   f.update(q:sum(w)//len(w))
-   
-  
+average_grades = {}
 
-print()
+for name, grades in students:
+    average = sum(grades) / len(grades)
+    average_grades[name] = average 
 
+best_student = max(average_grades, key=average_grades.get)
 
-print(f)
+best_average = average_grades[best_student]
+
+print(f"{best_student} имеет наивысший средний балл: {best_average}")
